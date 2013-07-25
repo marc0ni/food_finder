2b4f6eaf032f6bed3d7e7edb966efd15f62a08c2
Mark Lindamood <mark.lindamood@yahoo.com>
2013-07-25 10:52:03 -0700

    Initialized 'restaurant' class

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -5,42 +5,17 @@ class Restaurant
     @@filepath = File.join(APP_ROOT, path)
   end
   
-  attr_accessor :name, :cuisine, :price
-  
   def self.file_exists?
-    if @@filepath && File.exists?(@@filepath)
-      return true
-    else
-      return false
-    end
-    #class should know if the restaurant exists
-  end
-  
-  def self.file_usable?
-    return false unless @@filepath
-    return false unless File.exists?(@@filepath)
-    return false unless File.readable?(@@filepath)
-    return false unless File.writable?(@@filepath)
-    return true
+    # classs should know if the restaurant file exists
   end
   
   def self.create_file
-    #create the restaurant file
-    File.open(@@filepath, 'w') unless file_exists?
-    return file_usable?
+    # create the restaurant file
   end
   
   def self.saved_restaurants
-    #read the restaurant file
-    #return instances of restaurant
-  end
-  
-  def save
-    return false unless Restaurant.file_usable?
-    File.open(@@filepath, 'a') do |file|
-      file.puts "{[@name, @cuisine, @price].join("\t")}\n"
-    end
-    return true
+    # read the restaurant file
+    # return instances of restaurant
   end
   
 end
