cc22f3d5512353c94bbdb4ec188c42df45e13361
Mark Lindamood <mark.lindamood@yahoo.com>
2013-07-25 09:59:21 -0700

    Initial upload -- initialize 'init' and 'guide'

--- init.rb
+++ init.rb
@@ -5,9 +5,9 @@
 
 APP_ROOT = File.dirname(__FILE__)
 
-#require "#{APP_ROOT}/lib/guide"
-#require File.join(APP_ROOT, 'lib', 'guide.rb')
-#require File.join(APP_ROOT, 'lib', 'guide')
+# require "#{APP_ROOT}/lib/guide"
+# require File.join(APP_ROOT, 'lib', 'guide.rb')
+# require File.join(APP_ROOT, 'lib', 'guide')
 
 $:.unshift( File.join(APP_ROOT, 'lib'))
 require 'guide'
--- lib/guide.rb
+++ lib/guide.rb
@@ -1,93 +1,27 @@
-require 'restaurant'
 class Guide
-  class Config
-    @@actions = ['list', 'find', 'add', 'quit']
-    def self.actions; @@actions; end
-  end
   
   def initialize(path=nil)
     # locate the restaurant text file at path
-    Restaurant.filepath = path
-    if Restaurant.file_usable?
-      puts "Found restaurant file."
     # or create a new file
-    elsif Restaurant.create_file
-      puts "Created restaurant file."
     # exit if create fails
-    else
-      puts "Exiting.\n\n"
-      exit!
-    end
   end
   
   def launch!
     introduction
-    #action loop
-    result = nil
-    until result == :quit
-      #  what do you want to do? (list, find, add, quit)
-      action = get_action
-      result = do_action(user_response)
-    end
+    # action loop
+    #   what do you want to do? (list, find, add, quit)
+    #   do that action
+    # repeat until user quits
     conclusion
-  end
-  
-  def get_action
-    action = nil
-    #Keep asking for user input until we get a valid action
-    until Guide::Config.actions.include?(action)
-      puts "Actions: " + Guide::Config.actions.join(", ")
-      print ">"
-      user_response = gets.chomp
-      action = user_response.downcase.strip
-    end
-    return action
-  end
-  
-  def do_action(action)
-    case action
-    when 'list'
-      puts "Listing..."
-    when 'find'
-      puts "Finding..."
-    when 'add'
-      add
-    when 'quit'
-      return :quit
-    else
-      puts "\nI don't understand that command"
-    end
-  end
-  
-  def add
-    puts "\nAdd a restaurant\n\n".upcase
-    restaurant = Restaurant.new
-    
-    print "Restaurant name: "
-    restaurant.name = gets.chomp.strip
-    
-    print "Cuisine type: "
-    restaurant.cuisine = gets.chomp.strip
-    
-    print "Average price: "
-    restaurant.price =gets.chomp.strip
-    
-    if restaurant.save
-      puts "\nRestaurant Added"
-    else
-      puts "\nSave Error, Restaurant Not Added
-    end
-    
   end
   
   def introduction
     puts "\n\n<<< Welcome to the Food Finder >>>\n\n"
-    puts "This is an interactive guide to help you find the food you crave.\n\n"
   end
   
   def conclusion
     puts "\n<<< Goodbye and Bon Appetit! >>>\n\n\n"
   end
-    
+  
   
 end
