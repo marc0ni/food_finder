12162074c252a93590d282897a39c5d04d94dec8
Mark Lindamood <mark.lindamood@yahoo.com>
2013-07-27 10:40:30 -0700

    Limiting input on 'restaurant' class to valid actions (list, find, add, quit)

--- lib/guide.rb
+++ lib/guide.rb
@@ -35,10 +35,10 @@ class Guide
   def get_action
     action = nil
     # Keep asking for user input until we get a valid action
-    until Guide::Config.actions
+    until Guide::Config.actions.include?(action)
       puts "Actions: " + Guide::Config.actions.join(", ") if action
       print ">"
-      user_response = gets.chomp 
+      user_response = gets.chomp
       action = user_response.downcase.strip
     end
     return action
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -25,7 +25,7 @@ class Restaurant
   def self.create_file
     # create the restaurant file
     File.open(@@filepath, 'w') unless file_exists?
-    return file_exists?
+    return file_usable?
   end
   
   def self.saved_restaurants
